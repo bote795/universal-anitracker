095feff8ce21d4bd29fd1737d796dfe0de21cc99
Nicolas Botello <nbotell2@gmail.com>
2018-07-12 23:25:15 -0700

    basics for anilist provider

--- src/providers/anilist-provider.ts
+++ src/providers/anilist-provider.ts
@@ -1,9 +1,12 @@
 import { BasicProvider } from "../util/provider_interface";
+import { get, pickBy, identity } from "lodash";
 import {
   Anime,
   listEntry,
   inputAnime,
-  kitsuAddEntryPayload
+  AnilistAddEntryPayload,
+  AnilistUpdateEntryPayload,
+  AnilistMedia
 } from "../util/types";
 const Anilist = require('aniwrapper/node');
 class AnilistProvider implements BasicProvider {
@@ -12,19 +15,74 @@ class AnilistProvider implements BasicProvider {
     this.provider = new Anilist(accessToken);
   }
   getUserList() {
-    return this.provider.getUserList();
+    MediaListCollection.lists[0].entries;
+    return this.provider
+      .getUserList()
+      .then(({ MediaListCollection }: { MediaListCollection: any }) =>
+        get(MediaListCollection, "lists[0].entries")
+      )
+      .then((list: any) => {
+        return list.map((entry: anilisEntrysResponse) =>
+          this.outputNormalizeListEntry(entry)
+        );
+      });
   }
   searchAnime(name: string) {
     return this.provider.searchAnime(name);
   }
-  updateAnime(vars: any) {
-    return this.provider.updateAnime(vars);
+  updateAnime(vars: Partial<inputAnime>) {
+    const params: Partial<AnilistUpdateEntryPayload> = this.inputNormalizeAnime(
+      vars
+    );
+    return this.provider.updateAnime(params);
   }
-  removeAnime(id: any) {
+  removeAnime(id: Number) {
     return this.provider.removeAnime(id);
   }
-  addAnime(vars: any) {
-    return this.provider.addAnime(vars);
+  addAnime(vars: Partial<inputAnime>) {
+    const params = this.inputNormalizeAddAnime(vars);
+    return this.provider.addAnime(params);
+  }
+
+  //helpers
+  // ouput normalizers
+  outputNormalizeListEntry(entry: anilisEntrysResponse): listEntry {
+    const { id, progress, mediaId, media }:
+    { id: Number, progress: Number, mediaId :Number, media:AnilistMedia }
+      = entry;
+    return {
+      id,
+      progress,
+      anime: {
+        id: mediaId,
+        TotalEpisodes: media.episodes,
+        image: media.coverImage,
+        title: media.title
+      }
+    }
+  }
+  outputNormalizeAnime(anime: any): Anime {}
+
+  //input normalizers
+  inputNormalizeAddAnime(input: Partial<inputAnime>): AnilistAddEntryPayload {
+    const { status, progress, anime_id }: Partial<inputAnime> = input;
+    return {
+      mediaId: anime_id,
+      status,
+      progress
+    };
+  }
+  inputNormalizeAnime(
+    input: Partial<inputAnime>
+  ): Partial<AnilistUpdateEntryPayload> {
+    const { id, status, progress, anime_id }: Partial<inputAnime> = input;
+    const newType: Partial<AnilistUpdateEntryPayload> = {
+      mediaId: anime_id,
+      id,
+      status: status,
+      progress
+    };
+    return pickBy(newType, identity);
   }
 }
 export default AnilistProvider;
--- src/util/types.ts
+++ src/util/types.ts
@@ -32,6 +32,34 @@ export interface kitsuAddEntryPayload {
   anime: relationshipType;
   user: relationshipType;
 }
+
+export interface anilisEntrysResponse {
+  id: number;
+  mediaId: number;
+  progress: number;
+  media: AnilistMedia;
+}
+
+export interface AnilistMedia {
+  id: number;
+  episodes: number;
+  coverImage: CoverImage;
+  title: title;
+}
+export interface AnilistAddEntryPayload {
+  mediaId: number ;
+  status: string;
+  progress: number ;
+}
+
+export interface AnilistUpdateEntryPayload {
+  id: number | undefined;
+  mediaId: number | undefined;
+  status: string | undefined;
+  progress: number | undefined;
+}
+
+
 export interface relationshipType {
   id: string;
   type: string;
