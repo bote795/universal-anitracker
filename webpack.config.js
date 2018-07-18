a6d3c8a9bc9cfb94324f73b6675bf559b31b09e4
Nicolas Botello <nbotell2@gmail.com>
2018-07-17 22:36:59 -0700

    update the regex

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./lib/index.ts",
+  entry: './lib/index.ts',
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: "ts-loader",
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "node")
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'node'),
   },
-  target: "node",
-  mode: process.env.NODE_ENV === "production" ? "production" : "development"
+  target: 'node',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
